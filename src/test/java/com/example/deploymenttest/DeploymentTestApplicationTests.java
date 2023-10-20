9012e40d16fbba79cebd64fc6b6d2c69d1317767
Александр Гузий <aleksandrguzij@MacBook-Pro-Aleksandr.local>
2023-10-20 10:33:19 +0600

    Init commit

---
+++ src/test/java/com/example/deploymenttest/DeploymentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploymenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
